fdd533785b546eb87986b8d4343e0c87ed2c2c32
travisday <day.travis11@gmail.com>
2020-08-17 19:41:04 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:sup/components/LoadingCircle.dart';
+import 'package:sup/screens/Home.dart';
+import 'package:sup/screens/Login.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './services/auth.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
+
+  @override
+  Widget build(BuildContext context) {
+    print("drawing Main Page");
+
+    FirebaseAnalytics analytics = FirebaseAnalytics();
+
+    analytics.setCurrentScreen(screenName: "Main Screen").then((v) => {});
+
+    return MultiProvider(
+      providers: [
+        Provider<FirebaseAnalytics>.value(value: analytics),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+        ),
+        navigatorObservers: [
+          FirebaseAnalyticsObserver(analytics: analytics),
+        ],
+        initialRoute: "/",
+        home: FutureBuilder<FirebaseUser>(
+            future: AuthService().getUser,
+            builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                print("drawing main: target screen" +
+                    snapshot.connectionState.toString());
+                final bool loggedIn = snapshot.hasData;
+                return loggedIn ? HomePage() : LoginPage();
+              } else {
+                print("drawing main: loading circle" +
+                    snapshot.connectionState.toString());
+                return LoadingCircle();
+              }
+            }),
+      ),
+    );
+  }
+}
