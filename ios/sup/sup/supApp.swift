3579e94ed11a7277524297c5329e0dfb67006c01
travisday <day.travis11@gmail.com>
2021-05-19 09:24:06 -0700

    add ios proj

---
+++ ios/sup/sup/supApp.swift
@@ -0,0 +1,17 @@
+//
+//  supApp.swift
+//  sup
+//
+//  Created by Travis on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct supApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
